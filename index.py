eb20b35c4ef297def8c39f551f8d4155d28ed330
domtom <domtom1126@gmail.com>
2019-09-04 09:48:05 -0400

    install haversine to calculate distance of the ISS to the location of the user's city

--- index.py
+++ index.py
@@ -4,14 +4,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from geopy.geocoders import Nominatim
+from haversine import haversine, Unit
 
 geolocator = Nominatim(user_agent='ISS_Tracker')
-driver = webdriver.Chrome()
+driver = webdriver.Safari()
 url = driver.get('http://wsn.spaceflight.esa.int/iss/index_portal.php')
 
-user_location = geolocator.geocode(input("Enter your city "))
-print(user_location.latitude, user_location.longitude)
-
+user_city = geolocator.geocode(input("Enter your city "))
+user_location = user_city.latitude, user_city.longitude
+print(user_location)
 iss_latitude = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[text()='Latitude']//following::div[1]"))).get_attribute("innerHTML")
 
 iss_latitude_float = (float(iss_latitude.split(" ")[0].replace(",",".")))
@@ -23,6 +24,8 @@ iss_longitude_float = (float(iss_longitude.split(" ")[0].replace(",",".")))
 iss_speed = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[text()='Speed']//following::div[1]"))).get_attribute("innerHTML")
 print(iss_speed, iss_latitude_float, iss_longitude_float)
 
-
+iss_location = iss_latitude_float, iss_longitude_float
+distance = haversine(user_location, iss_location, unit=Unit.MILES)
+print(distance)
 driver.close()
 
