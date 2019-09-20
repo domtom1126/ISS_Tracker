5345fc5af92ddfc6d16fa54452f33df7306dc578
domtom <domtom1126@gmail.com>
2019-09-20 00:14:40 -0400

    Browser now donesn't display. Switched used browser to Chrome, replaced Safari.

--- index.py
+++ index.py
@@ -2,13 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from geopy.geocoders import Nominatim
 from haversine import haversine, Unit
 
 
 if __name__ == '__main__':
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
     geolocator = Nominatim(user_agent='ISS_Tracker')
-    driver = webdriver.Safari()
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     url = driver.get('http://wsn.spaceflight.esa.int/iss/index_portal.php')
 
     user_city = geolocator.geocode(input("Enter your city "))
